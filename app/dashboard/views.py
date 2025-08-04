629d9f382715acf09e788b44d92b9f1249cb547e
Jansen Simanullang <jansen.simanullang@gmail.com>
2025-08-04 19:19:28 +0700

    Simple Welcome to the Dashboard

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the dashboard!")
+    return HttpResponse("<h1>Welcome to the dashboard!</h1><p>This is the home page of the dashboard app.</p>")
