29062a41599c220b929af1ac8fb76692159768db
Jansen Simanullang <jansen.simanullang@gmail.com>
2025-08-04 19:23:31 +0700

    correct APP_ROOT path appended to sys.path

--- app/manage.py
+++ app/manage.py
@@ -4,46 +4,31 @@
 import os
 import sys
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-# Step 1: Get DJANGO_ENV early
-DJANGO_ENV = os.getenv('DJANGO_ENV')
-print(f"DJANGO_ENV: {DJANGO_ENV}")
+# Step 0: Set up the project root
+# Assuming this file is at app/manage.py, the project root will be app
+# This allows us to set the PYTHONPATH correctly
+# and load environment variables from the correct .env file.
+APP_ROOT = Path(__file__).resolve().parent 
+sys.path.append(str(APP_ROOT))
+# print(f"APPLICATION ROOT: {APP_ROOT}")
 
-# Step 2: Sanity check
-if DJANGO_ENV not in ['development', 'production']:
-    raise ValueError(f"Invalid DJANGO_ENV: {DJANGO_ENV}. Must be 'development' or 'production'.")
-
-# Step 3: Load corresponding .env file
-PROJECT_ROOT = Path(__file__).resolve().parent # Assuming manage.py is in the app directory
-# Ensure the PYTHONPATH includes the project root
-sys.path.append(str(PROJECT_ROOT))
-# print(f"Project root: {PROJECT_ROOT}")
-env_file = PROJECT_ROOT.parent / ('.env.development' if DJANGO_ENV == 'development' else '.env.production')
-# print(f"Loading environment variables from: {env_file}")
-load_dotenv(dotenv_path=env_file)
-
-# Step 4: Set Django settings module
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    f"core.settings.{DJANGO_ENV}"
-)
+from core.settings import DJANGO_ENV
 
 def main():
     """Run administrative tasks."""
+    # Ensure manage.py is the script name
+    sys.argv[0] = str(APP_ROOT / 'manage.py')
+    
+    # Ensure the working directory is set to the app directory
+    os.chdir(APP_ROOT)
+
     if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
-        # Ensure the working directory is set to the app directory
-        os.chdir(PROJECT_ROOT / 'core')
         print(f"Running server in {DJANGO_ENV} mode...")
     else:
         print(f"Running management command in {DJANGO_ENV} mode...")
 
-    # Step 5: Execute command line arguments
-    sys.argv[0] = str(PROJECT_ROOT / 'manage.py')  # Ensure manage.py is correctly referenced
-    if DJANGO_ENV == 'development':
-        sys.argv.append('--settings=core.settings.development')
-    else:
-        sys.argv.append('--settings=core.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -52,6 +37,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
     execute_from_command_line(sys.argv)
 
 
